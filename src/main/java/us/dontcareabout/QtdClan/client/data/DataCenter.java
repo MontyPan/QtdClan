1fd99b93f02a62cf270ba01242632f69bc470dc7
MontyPan <monty.pan@dontcareabout.us>
2021-08-08 00:41:42 +0800

    導入 DamageAnalyser、增加 seasonChange()

--- src/main/java/us/dontcareabout/QtdClan/client/data/DataCenter.java
+++ src/main/java/us/dontcareabout/QtdClan/client/data/DataCenter.java
@@ -1,12 +1,12 @@
 package us.dontcareabout.QtdClan.client.data;
 
-import java.util.ArrayList;
-
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.SimpleEventBus;
 
+import us.dontcareabout.QtdClan.client.common.DamageAnalyser;
 import us.dontcareabout.QtdClan.client.common.DateUtil;
 import us.dontcareabout.QtdClan.client.data.DamageReadyEvent.DamageReadyHandler;
+import us.dontcareabout.QtdClan.client.data.SeasonChangeEvent.SeasonChangeHandler;
 import us.dontcareabout.gst.client.data.SheetIdDao;
 import us.dontcareabout.gwt.client.Console;
 import us.dontcareabout.gwt.client.google.Sheet;
@@ -16,19 +16,16 @@ import us.dontcareabout.gwt.client.google.SheetHappen.Callback;
 public class DataCenter {
 	private final static SimpleEventBus eventBus = new SimpleEventBus();
 
-	public static int session;
-	public static ArrayList<Damage> damageList;
+	public static DamageAnalyser damageAnalyser;
 
 	public static void wantDamage(int session) {
-		DataCenter.session = session;
-
 		//第一個 tab 是 boss 進度，第二個是賽季紀錄
 		//第三個才是當前賽季、第 3+N 個是往前 N 個賽季...
 		int sessionIndex = 3 + DateUtil.nowSession() - session;
 		SheetHappen.<Damage>get(SheetIdDao.defaultValue(), sessionIndex, new Callback<Damage>() {
 			@Override
 			public void onSuccess(Sheet<Damage> gs) {
-				damageList = gs.getEntry();
+				damageAnalyser = new DamageAnalyser(session, gs.getEntry());
 				eventBus.fireEvent(new DamageReadyEvent());
 			}
 
@@ -41,5 +38,16 @@ public class DataCenter {
 
 	public static HandlerRegistration addDamageReady(DamageReadyHandler handler) {
 		return eventBus.addHandler(DamageReadyEvent.TYPE, handler);
+	}
+
+	////////
+
+	public static HandlerRegistration addSeasonChange(SeasonChangeHandler handler) {
+		return eventBus.addHandler(SeasonChangeEvent.TYPE, handler);
+	}
+
+	public static void seasonChange(int selectSeason) {
+		eventBus.fireEvent(new SeasonChangeEvent(selectSeason));
+		wantDamage(selectSeason);
 	}
 }
